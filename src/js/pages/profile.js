b4dd510e5ed7e7476574d2495f89bed7285cc389
Michael River Nilsson <mrn@michaelriver.dev>
2023-12-16 16:50:27 +0100

    Adds functionality to update profile avatar

--- src/js/pages/profile.js
+++ src/js/pages/profile.js
@@ -8,6 +8,7 @@ import { clearHTML } from "../components/clearHTML.js";
 import { checkIfProfileIsUsersProfile } from "../utils/checkifProfileIsUsersProfile.js";
 import { renderListings, renderProfileBidCard } from "../components/render.js";
 import { createMessage } from "../components/createMessage.js";
+import { onChangeAvatarFormSubmit } from "../utils/onCreateNewAvatarFormSubmit.js";
 
 export async function displayProfile() {
   try {
@@ -19,27 +20,29 @@ export async function displayProfile() {
       "#profileActiveBidsContainer",
     );
     const profileName = getProfileName();
-    console.log(profileName);
-
     const profileDetails = await getProfileDetails(profileName);
     const profileListings = await getProfileListings(profileName);
-
     const profileBids = await getProfileBids(profileName);
     const profileBidsDetails = profileBids.map(({ amount, listing }) => ({
       amount,
       listing,
       listingEndsAt: new Date(listing.endsAt),
     }));
-
     const todaysDate = new Date();
     const activeBidListings = profileBidsDetails.filter(
       ({ listingEndsAt }) => listingEndsAt > todaysDate,
     );
 
+    document.title = `${profileName} | AuctionHub`;
+
     displayLoggedInMenu();
     clearHTML(profileContainer);
     createProfileDetailsHTML(profileDetails, profileContainer);
     checkIfProfileIsUsersProfile();
+
+    const changeAvatarForm = document.querySelector("#changeAvatarForm");
+    changeAvatarForm.addEventListener("submit", onChangeAvatarFormSubmit);
+
     clearHTML(profileListingsContainer);
     if (profileListings.length > 0) {
       renderListings(profileListings, profileListingsContainer);
@@ -50,6 +53,7 @@ export async function displayProfile() {
         "You have no active listings.",
       );
     }
+
     clearHTML(profileActiveBidsContainer);
     if (activeBidListings.length > 0) {
       renderProfileBidCard(activeBidListings, profileActiveBidsContainer);
